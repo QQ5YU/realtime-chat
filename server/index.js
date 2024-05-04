f055d01cdcc1d4fb95d7eb6d2bfdbdf5e83dc23f
= <=>
2024-05-04 12:47:57 +0800

    register feature implement

--- server/index.js
+++ server/index.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+const UserModel = require("./models/User");
 const app = express();
+dotenv.config();
+
+const mongoURL = process.env.SERVER_MONGODB_URL || "not found";
+const jwtSecret = process.env.SERVER_JWT_SECRET || "not found";
+
+mongoose.connect(mongoURL).then(() => console.log("connected to MongoDB"));
 
 app.get("/", (req, res) => {
-  res.json("test ok");
+  res.json("get ok");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const newUser = await UserModel.create({ username, password });
+  console.log(username, password);
+  jwt.sign({ userId: newUser._id }, jwtSecret, (err, token) => {
+    if (err) throw err;
+    res.cookie("token", token).status(201).json("ok");
+  });
 });
 
 app.listen(4000, () => {
